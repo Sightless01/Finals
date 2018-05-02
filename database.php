9da71f249287640bd9dc091a8f7aba2f57a7107f
ricamelle <ricamelleemeterio@yahoo.com>
2018-05-02 23:04:50 +0800

    edited database

--- database.php
+++ database.php
@@ -8,69 +8,65 @@ function __construct(){
 $db = new DBphp();
 echo "ADMIN","</br>";
 $query1="SELECT * FROM admin;";
+echo "<table><tr><td>admin_id</td><td>username</td><td>password</td></tr>";
 $result=$db->query($query1);
-echo '<table cellpadding = "2" cellspacing = "2" class "db-table">';
-  echo "admin_id\tusername\tpassword\n","</br>";
 while ($row = $result->fetchArray()){
- 
- echo $row ['admin_id'] . 
- $row['username'] .
-$row['password'] .
- '<br/>';
+
+echo  '<tr><td>'.$row['admin_id'].'</td>
+<td>'.$row['username'].'</td>
+<td>'.$row['password'] .'</td>
+</tr>';
 }
 echo '</table>';
- echo "----------------------------------------------------","</br>";
+echo "----------------------------------------------------","</br>";
 
-$query1="SELECT * FROM Client;";
 echo "CLIENT","</br>";
+$query1="SELECT * FROM Client;";
 $result=$db->query($query1);
-echo '<table cellpadding = "5" cellspacing = "2" class "db-table">';
-  echo "client_id \tname\tproducts \tcontact\taddress\tpayment\n","</br>";
+ echo "<table><tr><td>client_id</td><td>name</td><td>tproducts</td><td>contact</td><td>address</td><td>payment</td></tr>";
 while ($row = $result->fetchArray()){
  
- echo $row ['client_id'] . 
- $row['name'] .
- $row['products'] .
-$row['contact'] .
- $row['address'] .
- $row['payment'] .
-
- '<br/>';
+echo '<tr><td>'.$row ['client_id'] . '</td>
+<td>'.$row['name'] .'</td>
+<td>'.$row['products'] .'</td>
+<td>'.$row['contact'] .'</td>
+<td>'.$row['address'] .'</td>
+<td>'.$row['payment'] .'</td>
+</tr>';
 }
 echo '</table>';
 echo  "----------------------------------------------------","</br>"; 
 
-$query1="SELECT * FROM Service_provider;";
 echo "SERVICE PROVIDER","</br>";
+$query1="SELECT * FROM Service_provider;";
 $result=$db->query($query1);
-echo '<table cellpadding = "3" cellspacing = "2" class "db-table">';
-  echo "id \tname \tproducts\n","</br>";
+echo "<table><tr><td>id </td><td>name</td><td>products</td></tr>";
 while ($row = $result->fetchArray()){
  
- echo $row ['id'] . 
- $row['name'] .
-$row['products'] .
-
- '<br/>';
+echo '<tr><td>'.$row ['id'] . '</td>
+<td>'.$row['name'] .'</td>
+<td>'.$row['products'] .'</td>
+</tr>';
 }
 echo '</table>';
 echo  "----------------------------------------------------","</br>"; 
 
-$query1="SELECT * FROM Products;";
 echo "PRODUCTS","</br>";
+$query1="SELECT * FROM Products;";
 $result=$db->query($query1);
-echo '<table cellpadding = "2" cellspacing = "2" class "db-table">';
-  echo "prod_id\tstyle\tcolor\tsize\tprice\n","</br>";
+echo "<table><tr><td>prod_id</td><td>style</td><td>color</td><td>size</td><td>price</td></tr>";
 while ($row = $result->fetchArray()){
  
- echo $row ['prod_id'] . 
- $row['style'] .
-$row['color'] .
-$row['size'] .
-$row['price'] .
- '<br/>';
+echo '<tr><td>'.$row ['prod_id'] .'</td> 
+<td>'.$row['style'] .'</td>
+<td>'.$row['color'] .'</td>
+<td>'.$row['size'] .'</td>
+<td>'.$row['price'] .'</td>
+ </tr>';
 }
 echo '</table>';
- echo "</br>";
+echo "</br>";
 
 $db->close();
+
+?>
