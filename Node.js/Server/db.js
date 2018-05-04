fe011f2478858922d41425d383847ba041c86eee
Ivan Torcuato <2137332@slu.edu.ph>
2018-05-05 04:47:47 +0800

    modification of the server

--- Node.js/Server/db.js
+++ Node.js/Server/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('course_website', null, null, {
+const sequelize = new Sequelize('../../database', null, null, {
   dialect: 'sqlite',
   operatorsAliases: false,
-  storage: './course-website.sqlite'
+  storage: '../../database.sqlite'
 });
 
 module.exports = sequelize;
