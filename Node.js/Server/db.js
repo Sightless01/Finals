10fe558d116203d04406a2b14431490f3a7995ca
Ivan Torcuato <2137332@slu.edu.ph>
2018-05-05 06:02:55 +0800

    minor

--- Node.js/Server/db.js
+++ Node.js/Server/db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('../../database', null, null, {
   dialect: 'sqlite',
   operatorsAliases: false,
-  storage: '../../database.sqlite'
+  storage: '../../database.db'
 });
 
 module.exports = sequelize;
