976bcffb592fbcd0703f3fec9c841c5834c46caf
Ivan Torcuato <2137332@slu.edu.ph>
2018-05-28 10:22:52 +0800

    fix adding of schema for database

--- database.sql
+++ database.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `database`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: database
+-- Host: localhost    Database: database
 -- ------------------------------------------------------
--- Server version	5.7.21
+-- Server version	5.5.5-10.1.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -204,4 +206,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-28  4:49:18
+-- Dump completed on 2018-05-28 10:22:30
